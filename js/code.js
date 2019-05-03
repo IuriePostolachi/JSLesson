9128924481ba3d19aca6794c55adef27d4f98717
IuriePostolachi <martolod14@gmail.com>
2019-05-03 15:00:26 +0300

    1 урок

--- js/code.js
+++ js/code.js
@@ -1,24 +1,37 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ""),
-    time = prompt(" Введите дату в формате", " YYYY-MM-DD "),
-    expensesAsck1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    expensesAsck2 = prompt("Во сколько обойдется?", "");
 
-console.log(money);
-console.log(time);
 
-let expenses = {
-  expensesAsck1,
-  expensesAsck2
-};
-console.log(expenses);
+function expensesCalc() {
+  let money = document.getElementById('money').value,
+      time = document.getElementById('time').value,
+      expensesAsck1 = document.getElementById('ask1').value,
+      expensesAsck2 = document.getElementById('ask2').value;
+  console.log(money);
+  console.log(time);
 
-let appData = {
+  let expenses = {
+      expensesAsck1,
+      expensesAsck2
+  };
+  console.log(expenses);
+
+  let appData = {
     moneyData: money,
     timeData: time,
     expensesData: expenses,
     optionalExpenses: {},
     income: {},
     savings: false
-};
+  };
+  console.log(appData);
+
+  let spending = (money - expensesAsck2) / 30;
+
+  let info = 'Ваш бюджет на 1 день : <b>' +  spending + '</b> рублей';  
+  document.getElementById('result').innerHTML = info;
+
+  
+}
+
+document.getElementById('calc').addEventListener('click', expensesCalc);
